d459a151aaee2dd002b7bdf7796724e2374b37ff
jeanl4930 <44269479+jeanl4930@users.noreply.github.com>
2018-11-12 04:46:17 -0500

    Add files via upload
    First submission

---
+++ P2T1_Jean.py
@@ -0,0 +1,14 @@
+#CTI110
+#M2T1
+#Jean
+#11/01/18
+studentName = "Jean"
+#studentGPA = 3.2
+#studentAge = 25
+print("Hello",studentName)
+#Get the projected total sales.
+total_sales = float(input("Enter the projected sales: "))
+#Calculate the profit
+profit=total_sales*0.23
+#Display the profit.
+print("The profit is $", profit)
