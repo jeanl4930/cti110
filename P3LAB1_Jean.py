608399fb7dc5981d0f34d3f52e11d4197bbf82ac
jeanl4930 <44269479+jeanl4930@users.noreply.github.com>
2018-12-13 11:49:35 -0500

    Add files via upload
    P3LAB is if/elif
    P3LAB1 is if/else

---
+++ P3LAB1_Jean.py
@@ -0,0 +1,31 @@
+#CTI110
+#P3LAB
+#Jean
+#11/15/18
+
+
+# This program takes a number grade and outputs a letter grade.
+# system uses 10-point grading scale
+
+A_score = 90
+B_score = 80
+C_score = 70
+D_score = 60
+    
+   
+    
+score = int(input('Enter grade: '))
+if score >= A_score:
+    print('Your grade is: A')
+else:
+    if score >= B_score:
+        print('Your grade is: B')
+    else:
+        if score >= C_score:
+            print('Your grade is: C')
+        else:
+            if score >= D_score:
+                print('Your grade is: D')
+            else:
+                print('Your grade is F.')
+                        
